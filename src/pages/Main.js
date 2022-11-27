d43dd94ef4413743082eebadf320076c96642bbc
ayseekmen <aysegucluekmen@gmail.com>
2022-11-28 00:02:33 +0300

    Add main page

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Main() {
+    return (
+        <SafeAreaView style={{backgroundColor: 'pink', flex: 1}}>
+            <Text>Hello Main DiaryApp</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Main;
